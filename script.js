ca4ec0d6e3aec1ec892b7c5e198985f1997c389f
MrChub467 <chubster467@gmail.com>
2024-11-19 13:51:35 -0600

    test

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const shelfHeight = document.querySelector("shelf1").getBoundingClientRect().height;
+const shelfWidth = document.querySelector("shelf1").getBoundingClientRect().width;
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,8 +12,94 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(book) {
-    const theBook = document.createElement("div");
+    const width = getBookDimensions(book.pages);
+    const shelf = doesBookFit(width);
+    const theShelf = document.querySelector(shelf);
+
+    const input = document.createElement("input");
+    setAttributes(input, {"type": "radio", "name": "book", "id": "book"})
+    theShelf.appendChild(input);
+
+    const label = document.createElement("label");
+    setAttributes(label, {"for": "book", "class": "book"})
+
+    const spine = document.createElement("div");
+    spine.classList.add("side", "spine");
+
+    const title = document.createElement("span");
+    title.classList.add("title");
+    spine.appendChild(title);
+
+    const author = document.createElement("span");    
+    author.classList.add("author");
+    spine.appendChild(author);
+    label.appendChild(spine);
     
+    const top = document.createElement("div");
+    top.classList.add("side", "top");
+    label.appendChild(top);
+
+    const content = document.createElement("div");
+    content.classList.add("side", "content");
+    content.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit tempore repellat nulla autem numquam enim at. Accusamus, at exercitationem dicta, libero beatae, odio architecto consectetur sapiente temporibus recusandae deserunt illum?"
+    label.appendChild(content);
+
+    const cover = document.createElement("div");
+    cover.classList.add("side", "cover");
+    label.appendChild(cover);
+
+    theShelf.appendChild(label);
+    
+}
+
+function getBookDimensions(pages) {
+    let width = 20
+    let height = (shelfHeight - 20) + "px";
+
+    switch(true) {
+        case pages > 500:
+            width = 60;
+            break;
+        case pages > 400:
+            width = 50;
+            break;
+        case pages > 300:
+            width = 40;
+            break;
+        case pages > 200:
+            width = 30;
+            break;
+    }
+
+    return width;
+
+}
+
+function doesBookFit(bookWidth) {
+    if (getShelfFill("shelf1") + bookWidth > shelfWidth) {
+        return "shelf1"
+    } else if (getShelfFill("shelf2") + bookWidth > shelfWidth) {
+        return "shelf2"
+    } else if (getShelfFill("shelf3") + bookWidth > shelfWidth) {
+        return "shelf3"
+    } else {
+        return "Bookshelf Full"
+    }
+}
+
+function getShelfFill(shelf) {
+    let children = document.querySelector(shelf).children;
+    let totalWidth = 0;
+    children.forEach(child => {
+        totalWidth += child.getBoundingClientRect().width;
+    });
+    return totalWidth;
+}
+
+function setAttributes(element, attributes) {
+    for(var key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
 }
 
 
@@ -26,25 +114,21 @@ form.addEventListener("submit", (e) => {
     const read = formData.get("read");
     const newBook = new Book(title, author, pages, read);
     
-    form.reset();
+    // form.reset();
     myLibrary.push(newBook);
-    addBookToLibrary(newBook);
+    // addBookToLibrary(newBook);
 
     console.dir(myLibrary);
 
 })
 
-// Will be used to have only one book selected at a time
-// const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+// Need function to put the book back when clicked because it is a radio input
 
-// checkboxes.forEach(checkbox => {
-//   checkbox.addEventListener('change', () => {
-//     if (checkbox.checked) {
-//       checkboxes.forEach(otherCheckbox => {
-//         if (otherCheckbox !== checkbox) {
-//           otherCheckbox.checked = false;
-//         }
-//       });
-//     }
-//   });
-// });
+// Need to edit addBookToLibrary function to add the book to the screen
+    // Function to get the size of the book given the number of pages
+    // Set attributes function
+    // Function to set a number to the elements for easy access
+    // Function to check to see if a shelf if full, if it is go to the next one
+
+// Maybe add a function to be able to drag books around
+
